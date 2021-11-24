45761802659f89df70dcba9c6403d1bc2b029d65
isaacschwab <schwab.isaac@gmail.com>
2021-11-24 14:15:03 -0600

    starting to swap to ethers and new contracts

--- src/FishFightSDK/index.ts
+++ src/FishFightSDK/index.ts
@@ -1,42 +1,45 @@
 // Harmony SDK
 import { Harmony, HarmonyExtension } from "@harmony-js/core"
-import { Contract } from "@harmony-js/contract"
+// import { Contract } from "@harmony-js/contract"
 
 // Contracts
-import Contracts from '@fishfight-one/contracts/FishFightContracts/testnet/contracts.json'
-import Web3 from "web3"
+// import Contracts from '@fishfight-one/contracts/FishFightContracts/testnet/contracts.json'
+import Contracts from '../contracts/contracts.json'
+// import Web3 from "web3"
+import { Web3Provider } from "@ethersproject/providers";
+import { Contract } from "@ethersproject/contracts"
 
 class FishFight {
-    provider: Harmony | HarmonyExtension | Web3
-    type: "web3" | "harmony" | "default"
+    provider: Harmony | HarmonyExtension | Web3Provider
+    type: "Web3Provider" | "harmony" | "default"
     factory: Contract
     fight: Contract
 
-    constructor(provider: Harmony | HarmonyExtension | Web3, type: "web3" | "harmony" | "default"){
+    constructor(provider: Harmony | HarmonyExtension | Web3Provider, type: "Web3Provider" | "harmony" | "default"){
         this.provider = provider
         this.type = type
         this.factory = this.setFactoryContract(this.provider, type)
         this.fight = this.setFightContract(this.provider, type)
     }
 
-    setFactoryContract = (provider: any, type: "web3" | "harmony" | "default") => {
+    setFactoryContract = (provider: any, type: "Web3Provider" | "harmony" | "default") => {
         if (type === "harmony" || type === "default" ) {
             return provider.contracts.createContract(Contracts.contracts.FishFactory.abi, Contracts.contracts.FishFactory.address)
         }
 
-        if (type === "web3") {
+        if (type === "Web3Provider") {
             return new provider.eth.Contract(Contracts.contracts.FishFactory.abi, Contracts.contracts.FishFactory.address)
         }
 
         return 'def'
     }
 
-    setFightContract = (provider: any, type: "web3" | "harmony" | "default") => {
+    setFightContract = (provider: any, type: "Web3Provider" | "harmony" | "default") => {
         if (type === "harmony" || type === "default" ) {
             return provider.contracts.createContract(Contracts.contracts.FishFight.abi, Contracts.contracts.FishFight.address)
         }
 
-        if (type === "web3") {
+        if (type === "Web3Provider") {
             return new provider.eth.Contract(Contracts.contracts.FishFight.abi, Contracts.contracts.FishFight.address)
         }
 
