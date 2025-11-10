76d5720096188f98c47ddefb0ac95750d7802252
Org-Edd <eddnorris@2bitdev.com>
2025-11-09 18:40:47 -0800

    Update index.tsx for React 18 compatibility
    - Changed from ReactDOM.render to createRoot().render() API
    - Updated imports to use react-dom/client
    - This completes the React 18 upgrade for proper webpack 5 compatibility

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,7 +15,10 @@ import { ContractWrapperProvider } from './context/contractWrapperContext';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
 	<React.StrictMode>
 		<Router>
 			<CookiesProvider>
@@ -34,9 +37,8 @@ ReactDOM.render(
 				</Web3ReactProvider>
 			</CookiesProvider>
 		</Router>
-		
-	</React.StrictMode>,
-	document.getElementById('root'),
+
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
